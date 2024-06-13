35206bdf7540042e000a98a16c21a6f7500d3ac7
Esteban SEMELLIER <esteban.semellier@mobiapps.fr>
2024-06-13 14:56:14 +0200

    fix CI/CD

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -1,16 +1,16 @@
 // MARK: - Configuration DB
 const config = {
     development: {
-      username: "estebansemellier",
-      password: null,
-      database: "database_development",
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       host: "127.0.0.1",
       dialect: "postgres",
     },
     test: {
-      username: "estebansemellier",
-      password: null,
-      database: "database_test",
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       host: "127.0.0.1",
       dialect: "postgres",
     },
